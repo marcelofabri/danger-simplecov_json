4fbf40b13d3b70c148a1d0608e584c437033c3ee
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-08-25 14:00:31 -0300

    Removing old code

--- spec/simplecov_json_spec.rb
+++ spec/simplecov_json_spec.rb
@@ -12,13 +12,7 @@ module Danger
         @simplecov = @dangerfile.simplecov
       end
 
-      # Some examples for writing tests
-      # You should replace these with your own.
-
       it 'Shows code coverage report' do
-        monday_date = Date.parse('2016-07-11')
-        allow(Date).to receive(:today).and_return monday_date
-
         @simplecov.report('spec/fixtures/coverage.json')
 
         expect(@dangerfile.status_report[:messages]).to eq(['Code coverage is now at 99.15% (1512/1525 lines)'])
