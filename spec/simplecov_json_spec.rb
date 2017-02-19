199b266a0bb6d3732d56b8c916db4b75a69ca139
RestlessThinker <penaflor@gmail.com>
2017-02-19 10:28:47 -0600

    Updated Readme, Version, Documentation and Tests

--- spec/simplecov_json_spec.rb
+++ spec/simplecov_json_spec.rb
@@ -18,6 +18,16 @@ module Danger
         expect(@dangerfile.status_report[:messages]).to eq(['Code coverage is now at 99.15% (1512/1525 lines)'])
       end
 
+      it 'Shows individual code coverage report' do
+        allow(@simplecov.git).to receive(:added_files).and_return([])
+        allow(@simplecov.git).to receive(:modified_files)
+          .and_return(['another_ruby_file.rb'])
+
+
+        @simplecov.individual_report('spec/fixtures/coverage.json', nil)
+        expect(@dangerfile.status_report[:markdowns][0].message).to eq("### Code Coverage\n\n| File                 | Coverage |\n|----------------------|----------|\n| another_ruby_file.rb | 20.00%   |")
+      end
+
       it 'Fails if code coverage not found' do
         @simplecov.report('spec/fixtures/missing_file.json')
 
